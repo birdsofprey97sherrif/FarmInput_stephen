7884927243b3062e709f632624d399d77a7359da
StephieThePlug <birdsofpreysherrif@gmail.com>
2025-05-13 14:53:24 +0300

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm fixed-top px-3">
       <div className="container-fluid">
         <NavLink className="navbar-brand d-flex align-items-center" to="/">
-          <img src="/logo.png" alt="Logo" height="40" className="me-2" />
+          <img src="/logo2.png" alt="Logo" height="40" className="me-2" />
           <span className="fw-bold text-info fs-4">Alpha AgriGear</span>
         </NavLink>
 
